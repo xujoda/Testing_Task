4242d53e1a8a0d619aa12d741f643245b9fce33c
Vadim <xujoda@gmail.com>
2023-04-04 20:49:02 +0500

    Добавьте файлы проекта.

---
+++ Testing_Task/Ribbon1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Interop.PowerPoint;
+using System.Drawing;
+using System.Runtime.InteropServices.ComTypes;
+using System.Windows.Forms;
+
+// TODO:  Выполните эти шаги, чтобы активировать элемент XML ленты:
+
+// 1: Скопируйте следующий блок кода в класс ThisAddin, ThisWorkbook или ThisDocument.
+
+//  protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+//  {
+//      return new Ribbon1();
+//  }
+
+// 2. Создайте методы обратного вызова в области "Обратные вызовы ленты" этого класса, чтобы обрабатывать действия
+//    пользователя, например нажатие кнопки. Примечание: если эта лента экспортирована из конструктора ленты,
+//    переместите свой код из обработчиков событий в методы обратного вызова и модифицируйте этот код, чтобы работать с
+//    моделью программирования расширения ленты (RibbonX).
+
+// 3. Назначьте атрибуты тегам элементов управления в XML-файле ленты, чтобы идентифицировать соответствующие методы обратного вызова в своем коде.  
+
+// Дополнительные сведения можно найти в XML-документации для ленты в справке набора средств Visual Studio для Office.
+
+
+namespace Testing_Task
+{
+    [ComVisible(true)]
+    public class Ribbon1 : Office.IRibbonExtensibility
+    {
+        private Office.IRibbonUI ribbon;
+
+        public Ribbon1()
+        {
+        }
+
+        #region Элементы IRibbonExtensibility
+
+        public string GetCustomUI(string ribbonID)
+        {
+            return GetResourceText("Testing_Task.Ribbon1.xml");
+        }
+
+        #endregion
+
+        #region Обратные вызовы ленты
+        //Информацию о методах создания обратного вызова см. здесь. Дополнительные сведения о методах добавления обратного вызова см. по ссылке https://go.microsoft.com/fwlink/?LinkID=271226
+
+        public void Ribbon_Load(Office.IRibbonUI ribbonUI)
+        {
+            this.ribbon = ribbonUI;
+        }
+
+        public void OnAddSlideButtonClicked(Office.IRibbonControl control)
+        {
+            int slidesCount = Globals.ThisAddIn.Application.ActivePresentation.Slides.Count;
+            PowerPoint.Slide newSlide = Globals.ThisAddIn.Application.ActivePresentation.Slides.Add(slidesCount, PowerPoint.PpSlideLayout.ppLayoutBlank);
+
+            try
+            {
+            string imageUrl = "https://the-decoder.com/wp-content/uploads/2022/09/ai_xbox_midjourney.png";
+            PowerPoint.Shape shape = newSlide.Shapes.AddPicture(imageUrl, Microsoft.Office.Core.MsoTriState.msoFalse, Microsoft.Office.Core.MsoTriState.msoTrue, 0, 0, newSlide.Master.Width, newSlide.Master.Height);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Сожалею, видимо, что-то пошло не по плану :( Ошибка: " + ex.Message);
+            }
+
+        }
+
+
+        #endregion
+
+        #region Вспомогательные методы
+
+        private static string GetResourceText(string resourceName)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            string[] resourceNames = asm.GetManifestResourceNames();
+            for (int i = 0; i < resourceNames.Length; ++i)
+            {
+                if (string.Compare(resourceName, resourceNames[i], StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    using (StreamReader resourceReader = new StreamReader(asm.GetManifestResourceStream(resourceNames[i])))
+                    {
+                        if (resourceReader != null)
+                        {
+                            return resourceReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        #endregion
+    }
+}
---
+++ Testing_Task/ThisAddIn.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+using Office = Microsoft.Office.Core;
+using System.Windows.Forms;
+using Testing_Task.Properties;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Testing_Task
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region Код, автоматически созданный VSTO
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+
+        #endregion
+        
+        protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+        {
+            return new Ribbon1();
+        }
+    }
+}
